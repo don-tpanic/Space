135ff7cff4ed4cf38a0969af25b22bf15f443214
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2023-02-25 15:00:22 +0000

    update code: consistent with output API

--- dimension_reduction.py
+++ dimension_reduction.py
@@ -31,18 +31,18 @@ def compute_components(X, reduction_method, reduction_hparams, random_state=999)
         )
         X_latent = kpca.fit_transform(X)
         explained_variance_ratio = kpca.lambdas_ / np.sum(kpca.lambdas_)
-        return X_latent, explained_variance_ratio
+        return X_latent, explained_variance_ratio, kpca
     
     elif reduction_method == 'nmf':
         print(f'running NMF...')
         nmf = NMF(n_components=np.min(X.shape), random_state=random_state)
         X_latent = nmf.fit_transform(X)
-        return X_latent, None
+        return X_latent, None, nmf
 
     elif reduction_method == 'ica':
         ica = FastICA(n_components=np.min(X.shape), random_state=random_state)
         X_latent = ica.fit_transform(X)
-        return X_latent, None
+        return X_latent, None, ica
 
     elif reduction_method == 'avgmax':
         print(f'running avgmax...')
@@ -51,7 +51,7 @@ def compute_components(X, reduction_method, reduction_hparams, random_state=999)
         col_ranks_desc = np.argsort(np.mean(X, axis=0))[::-1]
         print(f'col_ranks_desc.shape: {col_ranks_desc.shape}')
         X_latent = X[:, col_ranks_desc]
-        return X_latent, None
+        return X_latent, None, None
     
     elif reduction_method == 'maxvar':
         print(f'running maxvar...')
@@ -60,8 +60,8 @@ def compute_components(X, reduction_method, reduction_hparams, random_state=999)
         col_ranks_desc = np.argsort(np.var(X, axis=0))[::-1]
         print(f'col_ranks_desc.shape: {col_ranks_desc.shape}')
         X_latent = X[:, col_ranks_desc]
-        return X_latent, None
-        
+        return X_latent, None, None
+
 
 def compute_component_matrix(
         X, reduction_method, 
