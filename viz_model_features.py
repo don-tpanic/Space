93b1ae19cd3c69ca3d474b7eb6e1b72c26d95cb0
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2023-05-15 10:37:40 +0100

    update code: run a few more viz analyses

--- viz_model_features.py
+++ viz_model_features.py
@@ -370,10 +370,9 @@ if __name__ == '__main__':
     experiment = 'viz'
     envs = ['env28_r24']
     movement_modes = ['2d']
-    sampling_rates = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+    sampling_rates = [0.01, 0.1, 0.5]
     random_seeds = [42, 1234, 999]
-    # model_names = ['simclrv2_r50_1x_sk0', 'resnet50', 'vgg16']
-    model_names = ['vgg16']
+    model_names = ['simclrv2_r50_1x_sk0', 'resnet50', 'vgg16']
     moving_trajectories = ['uniform']
     decoding_model_choices = [
         {'name': 'ridge_regression', 'hparams': 1.0},
