60dccbb933d8911ae0e005b2491233f5010e476d
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2023-06-01 09:38:14 +0100

    add code: options for extra inspect_units sub-experiments

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def load_results_path(
             f'{decoding_model_name}_{decoding_model_hparams}/'\
             f'{output_layer}/sr{sampling_rate}/seed{random_seed}'
         
-    elif experiment in ['viz', 'fields_info'] \
+    elif experiment in ['viz', 'fields_info', 'gridness', 'unit_chart'] \
             and reference_experiment == 'loc_n_rot':
         results_path = \
             f'results/{unity_env}/{movement_mode}/{moving_trajectory}/'\
@@ -100,7 +100,7 @@ def load_figs_path(
             f'{decoding_model_name}_{decoding_model_hparams}/'\
             f'{output_layer}/sr{sampling_rate}/seed{random_seed}'
         
-    elif experiment in ['viz', 'fields_info', 'similarity'] \
+    elif experiment in ['viz', 'fields_info', 'gridness', 'unit_chart'] \
             and reference_experiment == 'loc_n_rot':
         figs_path = \
             f'figs/{unity_env}/{movement_mode}/{moving_trajectory}/'\
