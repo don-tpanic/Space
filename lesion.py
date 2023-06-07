de3fa70fd08c43437df62d727b589e9de4168a98
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2023-06-07 14:22:52 +0100

    WIP: add doc; option to lesion by coef

--- lesion.py
+++ lesion.py
@@ -32,82 +32,114 @@ def lesion(
                     
     Args:
         experiment='unit_chart' 
-            is always the case because in order to lesion
-            we will have done unit charting first.
-    """
-    charted_info = [
-                    'dead',
-                    'num_clusters', 
-                    'num_pixels_in_clusters', 
-                    'max_value_in_clusters', 
-                    'mean_value_in_clusters', 
-                    'var_value_in_clusters',
-                    'entire_map_mean',
-                    'entire_map_var',
-                    'gridness',
-                    'borderness',
-                ]
-    
-    # load unit chart info
-    results_path = utils.load_results_path(
-        config=config,
-        experiment=experiment,
-        moving_trajectory=moving_trajectory,
-    )
-    unit_chart_info = np.load(
-        f'{results_path}/unit_chart.npy', allow_pickle=True)
-    logging.info(f'unit_chart_info.shape: {unit_chart_info.shape}')
+            which is when we want to lesion based on unit chart.
+            To run this setting, we need to run the corresponding
+            unit_chart script first to produce the unit chart,
+            which are saved in
+                `results/./inspect_units/./unit_chart.npy`
+            
+        OR 
 
-    units_to_lesion_scores = []
-    units_to_lesion_indices = []
-    if 'gridness' in feature_selection:
-        # e.g. For `l2+lesion_borderness_0.37_top_0.1`
-        #       we extract the thr=0.37, rank=top, and ratio=0.1
-        #       and for units with gridness > 0.37, we lesion 
-        #       the top 10% of them. 
-        # NOTE: I suppose there is a chance there aren't any qualified
-        # but unlikely. In which case, need to adjust thr.
-        thr = float(feature_selection.split('_')[2])
-        rank = feature_selection.split('_')[3]
-        ratio = float(feature_selection.split('_')[4])
-        for unit_index in range(unit_chart_info.shape[0]):
-            # record units have gridness > thr
-            unit_score = unit_chart_info[unit_index, charted_info.index('gridness')]
-            if unit_score > thr:
-                units_to_lesion_scores.append(unit_score)
-                units_to_lesion_indices.append(unit_index)
+        experiment='loc_n_rot' or 'border_dist'
+            which is when we want to lesion based on coefs
+            learned from specific decoding models.
+            To run this setting, we need to run the corresponding
+            decoding model first to produce the coefs, which are
+            saved in 
+                `results/./loc_n_rot/./res.npy` or 
+                `results/./border_dist/./res.npy`
+
+        NOTE(ken), currently, when experiment != 'unit_chart', loading the 
+        coefs require extra info such as sampling rate and random seed as 
+        the decoding models are not `general`, unlike the unit chart which
+        is produced from `general` outputs. Although, this may be subject to
+        change, for example, we might decide that we need to chart units based 
+        on their firing conditioned on sampled locations not the entire space.
+    """
+    if experiment == 'unit_chart':
+        charted_info = [
+                        'dead',
+                        'num_clusters', 
+                        'num_pixels_in_clusters', 
+                        'max_value_in_clusters', 
+                        'mean_value_in_clusters', 
+                        'var_value_in_clusters',
+                        'entire_map_mean',
+                        'entire_map_var',
+                        'gridness',
+                        'borderness',
+                    ]
         
-        units_to_lesion_scores = np.array(units_to_lesion_scores)
-        units_to_lesion_indices = np.array(units_to_lesion_indices)
-                    
-        # lesion the top ratio% of units
-        if rank == 'top':
-            # sort from high to slow
-            units_to_lesion_indices = units_to_lesion_indices[np.argsort(units_to_lesion_scores)][::-1]
-            num_units_to_lesion = int(len(units_to_lesion_indices) * ratio)
-            units_to_lesion_indices = units_to_lesion_indices[num_units_to_lesion:]
-        
-    elif 'borderness' in feature_selection:
-        thr = float(feature_selection.split('_')[2])
-        rank = feature_selection.split('_')[3]
-        ratio = float(feature_selection.split('_')[4])
-        for unit_index in range(unit_chart_info.shape[0]):
-            # record units have borderness > thr
-            unit_score = unit_chart_info[unit_index, charted_info.index('borderness')]
-            if unit_score > thr:
-                units_to_lesion_scores.append(unit_score)
-                units_to_lesion_indices.append(unit_index)
-        
-        units_to_lesion_scores = np.array(units_to_lesion_scores)
-        units_to_lesion_indices = np.array(units_to_lesion_indices)
-        
-        # lesion the top ratio% of units
-        if rank == 'top':
-            # sort from high to slow
-            units_to_lesion_indices = units_to_lesion_indices[np.argsort(units_to_lesion_scores)][::-1]
-            num_units_to_lesion = int(len(units_to_lesion_indices) * ratio)
-            units_to_lesion_indices = units_to_lesion_indices[num_units_to_lesion:]
+        # load unit chart info
+        results_path = utils.load_results_path(
+            config=config,
+            experiment=experiment,
+            moving_trajectory=moving_trajectory,
+        )
+        unit_chart_info = np.load(
+            f'{results_path}/unit_chart.npy', allow_pickle=True)
+        logging.info(f'unit_chart_info.shape: {unit_chart_info.shape}')
+
+        units_to_lesion_scores = []
+        units_to_lesion_indices = []
+        if 'gridness' in feature_selection:
+            # e.g. For `l2+lesion_borderness_0.37_top_0.1`
+            #       we extract the thr=0.37, rank=top, and ratio=0.1
+            #       and for units with gridness > 0.37, we lesion 
+            #       the top 10% of them. 
+            # NOTE: I suppose there is a chance there aren't any qualified
+            # but unlikely. In which case, need to adjust thr.
+            thr = float(feature_selection.split('_')[2])
+            rank = feature_selection.split('_')[3]
+            ratio = float(feature_selection.split('_')[4])
+            for unit_index in range(unit_chart_info.shape[0]):
+                # record units have gridness > thr
+                unit_score = unit_chart_info[unit_index, charted_info.index('gridness')]
+                if unit_score > thr:
+                    units_to_lesion_scores.append(unit_score)
+                    units_to_lesion_indices.append(unit_index)
+            
+            units_to_lesion_scores = np.array(units_to_lesion_scores)
+            units_to_lesion_indices = np.array(units_to_lesion_indices)
+                        
+            # lesion the top ratio% of units
+            if rank == 'top':
+                # sort from high to slow
+                units_to_lesion_indices = units_to_lesion_indices[np.argsort(units_to_lesion_scores)][::-1]
+                num_units_to_lesion = int(len(units_to_lesion_indices) * ratio)
+                units_to_lesion_indices = units_to_lesion_indices[num_units_to_lesion:]
+            
+        elif 'borderness' in feature_selection:
+            thr = float(feature_selection.split('_')[2])
+            rank = feature_selection.split('_')[3]
+            ratio = float(feature_selection.split('_')[4])
+            for unit_index in range(unit_chart_info.shape[0]):
+                # record units have borderness > thr
+                unit_score = unit_chart_info[unit_index, charted_info.index('borderness')]
+                if unit_score > thr:
+                    units_to_lesion_scores.append(unit_score)
+                    units_to_lesion_indices.append(unit_index)
+            
+            units_to_lesion_scores = np.array(units_to_lesion_scores)
+            units_to_lesion_indices = np.array(units_to_lesion_indices)
+            
+            # lesion the top ratio% of units
+            if rank == 'top':
+                # sort from high to slow
+                units_to_lesion_indices = units_to_lesion_indices[np.argsort(units_to_lesion_scores)][::-1]
+                num_units_to_lesion = int(len(units_to_lesion_indices) * ratio)
+                units_to_lesion_indices = units_to_lesion_indices[num_units_to_lesion:]
     
+    elif experiment == 'loc_n_rot' or experiment == 'border_dist':
+        # load coef info
+        results_path = utils.load_results_path(
+            config=config,
+            experiment=experiment,
+            moving_trajectory=moving_trajectory,
+        )
+        raise NotImplementedError
+
+
     # lesion based on `units_to_lesion_indices`
     # keep the rest columns
     lesioned_model_reps = np.delete(model_reps, units_to_lesion_indices, axis=1)
