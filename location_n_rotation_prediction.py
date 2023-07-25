af437d5fe222bfdb1858697afa7a825a6ecbdf1a
Xiaoliang Luo <ken.xiaoliang@gmail.com>
2023-07-25 12:46:36 +0100

    update figs: lesion by directioness inc. logits layer

--- location_n_rotation_prediction.py
+++ location_n_rotation_prediction.py
@@ -1348,11 +1348,11 @@ if __name__ == '__main__':
     moving_trajectories = ['uniform']
     decoding_model_choices = [{'name': 'ridge_regression', 'hparams': 1.0}]
     experiment = 'loc_n_rot'
-    reference_experiment = 'loc_n_rot'   #'loc_n_rot|border_dist|unit_chart'
-    metric = 'coef'
-    thr = 'thr'                            # if metric=='coef', thr='thr'
-    rank = 'random'
-    target = '_loc'                          # if metric=='coef', target='_loc|_rot
+    reference_experiment = 'unit_chart'   #'loc_n_rot|border_dist|unit_chart'
+    metric = 'directioness'
+    thr = '0'                            # if metric=='coef', thr='thr', else '0'
+    rank = 'top'
+    target = ''                          # if metric=='coef', target='_loc|_rot', else ''
     feature_selections = [
         'l2',
         f'l2+lesion_{metric}_{thr}_{rank}_0.1{target}',
