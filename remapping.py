98fb77a65572c6b1570466d45eaeed0cf15c4087
Xiaoliang (Ken) Luo <ken@love22.pals.ucl.ac.uk>
2024-07-21 13:15:17 +0100

    added code: unit type change overall plotter

--- remapping.py
+++ remapping.py
@@ -54,8 +54,102 @@ def _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory):
     plt.savefig("remapping.png")
 
 
+def _plot_between_envs_unit_types_change(config_version_1, config_version_2, experiment, moving_trajectory):
+    """
+    For two contrasting envs, each env's units follow the following types:
+    
+    env_1 = {
+        'n_dead_units': n_dead_units,
+        'place_border_direction_cells_indices': place_border_direction_cells_indices,
+        'place_and_border_not_direction_cells_indices': place_and_border_not_direction_cells_indices,
+        'place_and_direction_not_border_cells_indices': place_and_direction_not_border_cells_indices,
+        'border_and_direction_not_place_cells_indices': border_and_direction_not_place_cells_indices,
+        'exclusive_place_cells_indices': exclusive_place_cells_indices,
+        'exclusive_border_cells_indices': exclusive_border_cells_indices,
+        'exclusive_direction_cells_indices': exclusive_direction_cells_indices,
+    }
+
+    env_2 = {
+        ...
+    }
+
+    Based on `exclusive_place_cells_indices` of env_1, compare env_2
+    to find:
+        1. Number of units belong to `n_dead_units`
+        2. Number of units belong to `place_border_direction_cells_indices`
+        3. Number of units belong to `place_and_border_not_direction_cells_indices`
+        4. Number of units belong to `place_and_direction_not_border_cells_indices`
+        5. Number of units belong to `border_and_direction_not_place_cells_indices`
+        6. Number of units belong to `exclusive_place_cells_indices`
+    """
+    config_1 = utils.load_config(config_version_1)
+    results_path_1 = utils.load_results_path(
+        config=config_1,
+        experiment=experiment,
+        moving_trajectory=moving_trajectory,
+    )
+    fpath_1 = f'{results_path_1}/unit_chart.npy'
+    unit_chart_1 = np.load(fpath_1, allow_pickle=True)
+    indices_1 = umc._unit_chart_type_classification(unit_chart_1)
+
+    config_2 = utils.load_config(config_version_2)
+    results_path_2 = utils.load_results_path(
+        config=config_2,
+        experiment=experiment,
+        moving_trajectory=moving_trajectory,
+    )
+    fpath_2 = f'{results_path_2}/unit_chart.npy'
+    unit_chart_2 = np.load(fpath_2, allow_pickle=True)
+    indices_2 = umc._unit_chart_type_classification(unit_chart_2)
+
+    exclusive_place_cells_indices_1 = indices_1['exclusive_place_cells_indices']
+
+    # Initialize a dictionary to store the results
+    results = {
+        'dead_units_indices': 0,
+        'place_border_direction_cells': 0,
+        'place_and_border_not_direction_cells': 0,
+        'place_and_direction_not_border_cells': 0,
+        'border_and_direction_not_place_cells': 0,
+        'exclusive_place_cells': 0
+    }
+
+    # Iterate through the exclusive place cells from env_1
+    for idx in exclusive_place_cells_indices_1:
+        if idx in indices_2['dead_units_indices']:
+            results['dead_units_indices'] += 1
+        elif idx in indices_2['place_border_direction_cells_indices']:
+            results['place_border_direction_cells'] += 1
+        elif idx in indices_2['place_and_border_not_direction_cells_indices']:
+            results['place_and_border_not_direction_cells'] += 1
+        elif idx in indices_2['place_and_direction_not_border_cells_indices']:
+            results['place_and_direction_not_border_cells'] += 1
+        elif idx in indices_2['border_and_direction_not_place_cells_indices']:
+            results['border_and_direction_not_place_cells'] += 1
+        elif idx in indices_2['exclusive_place_cells_indices']:
+            results['exclusive_place_cells'] += 1
+    
+    # Calculate percentages
+    total_cells = len(exclusive_place_cells_indices_1)
+    percentages = {key: (value / total_cells) * 100 for key, value in results.items()}
+
+    # Create a bar plot
+    plt.figure(figsize=(12, 6))
+    plt.bar(percentages.keys(), percentages.values())
+    plt.title(f"Change in Unit Types from {config_version_1} to {config_version_2}")
+    plt.xlabel("Unit Types")
+    plt.ylabel("Percentage of Units")
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+
+    # Save the plot
+    plot_path = f'{figs_dir}/unit_type_change_plot_{config_version_1}-{config_version_2}.png'
+    plt.savefig(plot_path)
+
+
 def main(configs, experiment, moving_trajectory):
-    _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory)
+    # _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory)
+    _plot_between_envs_unit_types_change(configs[0], configs[2], experiment, moving_trajectory)
 
 
 if __name__ == '__main__':
@@ -74,5 +168,6 @@ if __name__ == '__main__':
         "env37_r24_2d_vgg16_fc2",
         "env38_r24_2d_vgg16_fc2",
     ]
+    figs_dir = "figs/remapping"
 
     main(configs, experiment, moving_trajectory)
