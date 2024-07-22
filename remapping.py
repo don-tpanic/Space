f3afe26099686ed21289581889f562ebc7090b83
Xiaoliang (Ken) Luo <ken@love22.pals.ucl.ac.uk>
2024-07-22 11:09:11 +0100

    added code: new plotter shows unit type to unit type changes between envs

--- remapping.py
+++ remapping.py
@@ -6,6 +6,13 @@ import time
 import utils
 import unit_metric_computers as umc
 
+plt.rcParams.update(
+    {
+        'font.size': 22, 
+        'font.family': 'sans-serif',
+        'font.sans-serif': 'Arial',
+    }
+)
 
 def _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory):
     unit_charts = []
@@ -56,31 +63,8 @@ def _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory):
 
 def _plot_between_envs_unit_types_change(config_version_1, config_version_2, experiment, moving_trajectory):
     """
-    For two contrasting envs, each env's units follow the following types:
-    
-    env_1 = {
-        'n_dead_units': n_dead_units,
-        'place_border_direction_cells_indices': place_border_direction_cells_indices,
-        'place_and_border_not_direction_cells_indices': place_and_border_not_direction_cells_indices,
-        'place_and_direction_not_border_cells_indices': place_and_direction_not_border_cells_indices,
-        'border_and_direction_not_place_cells_indices': border_and_direction_not_place_cells_indices,
-        'exclusive_place_cells_indices': exclusive_place_cells_indices,
-        'exclusive_border_cells_indices': exclusive_border_cells_indices,
-        'exclusive_direction_cells_indices': exclusive_direction_cells_indices,
-    }
-
-    env_2 = {
-        ...
-    }
-
-    Based on `exclusive_place_cells_indices` of env_1, compare env_2
-    to find:
-        1. Number of units belong to `n_dead_units`
-        2. Number of units belong to `place_border_direction_cells_indices`
-        3. Number of units belong to `place_and_border_not_direction_cells_indices`
-        4. Number of units belong to `place_and_direction_not_border_cells_indices`
-        5. Number of units belong to `border_and_direction_not_place_cells_indices`
-        6. Number of units belong to `exclusive_place_cells_indices`
+    For two contrasting envs, compare how unit types change from env_1 to env_2.
+    For each unit type in env_1, plot the percentage of units that have changed to each type in env_2.
     """
     config_1 = utils.load_config(config_version_1)
     results_path_1 = utils.load_results_path(
@@ -102,54 +86,101 @@ def _plot_between_envs_unit_types_change(config_version_1, config_version_2, exp
     unit_chart_2 = np.load(fpath_2, allow_pickle=True)
     indices_2 = umc._unit_chart_type_classification(unit_chart_2)
 
-    exclusive_place_cells_indices_1 = indices_1['exclusive_place_cells_indices']
-
-    # Initialize a dictionary to store the results
-    results = {
-        'dead_units_indices': 0,
-        'place_border_direction_cells': 0,
-        'place_and_border_not_direction_cells': 0,
-        'place_and_direction_not_border_cells': 0,
-        'border_and_direction_not_place_cells': 0,
-        'exclusive_place_cells': 0
+    unit_types = {
+        'dead_units_indices': 'Inactive',
+        'place_border_direction_cells_indices': 'P+B+D',
+        'place_and_border_not_direction_cells_indices': 'P+B',
+        'place_and_direction_not_border_cells_indices': 'P+D',
+        'border_and_direction_not_place_cells_indices': 'B+D',
+        'exclusive_place_cells_indices': 'P',
+        'exclusive_border_cells_indices': 'B',
+        'exclusive_direction_cells_indices': 'D',
+        'active_no_type_indices': 'No Type'
     }
 
-    # Iterate through the exclusive place cells from env_1
-    for idx in exclusive_place_cells_indices_1:
-        if idx in indices_2['dead_units_indices']:
-            results['dead_units_indices'] += 1
-        elif idx in indices_2['place_border_direction_cells_indices']:
-            results['place_border_direction_cells'] += 1
-        elif idx in indices_2['place_and_border_not_direction_cells_indices']:
-            results['place_and_border_not_direction_cells'] += 1
-        elif idx in indices_2['place_and_direction_not_border_cells_indices']:
-            results['place_and_direction_not_border_cells'] += 1
-        elif idx in indices_2['border_and_direction_not_place_cells_indices']:
-            results['border_and_direction_not_place_cells'] += 1
-        elif idx in indices_2['exclusive_place_cells_indices']:
-            results['exclusive_place_cells'] += 1
-    
-    # Calculate percentages
-    total_cells = len(exclusive_place_cells_indices_1)
-    percentages = {key: (value / total_cells) * 100 for key, value in results.items()}
+    type_change_percentages = {}
+    # e.g., {"Inactive": {"Inactive": 1, "P+B+D": 2, "P+B": 3, "P+D": 4, "B+D": 5, "P": 6}}
 
-    # Create a bar plot
-    plt.figure(figsize=(12, 6))
-    plt.bar(percentages.keys(), percentages.values())
-    plt.title(f"Change in Unit Types from {config_version_1} to {config_version_2}")
-    plt.xlabel("Unit Types")
-    plt.ylabel("Percentage of Units")
-    plt.xticks(rotation=45, ha='right')
-    plt.tight_layout()
+    for i, unit_type_1 in enumerate(unit_types.keys()):
+        results = {}
+        # e.g.  = {'Inactive': 1, 'P+B+D': 2, 'P+B': 3, 'P+D': 4, 'B+D': 5, 'P': 6}
+        
+        # Count how many units of type_1 changed to each type in env_2
+        for idx in indices_1[unit_type_1]:
+            for unit_type_2 in unit_types:
+                if idx in indices_2[unit_type_2]:
+                    unit_type_2_print = unit_types[unit_type_2]
+                    results[unit_type_2_print] = results.get(unit_type_2_print, 0) + 1
+                    break  # once found, can safety break because each unit can only change to one type
+        
+        # Calculate percentages
+        total_cells = len(indices_1[unit_type_1])
+        if total_cells > 0:
+            percentages = {key: (value / total_cells) * 100 for key, value in results.items()}
+        
+            # Store the percentages
+            type_change_percentages[unit_types[unit_type_1]] = percentages
+
+    # Determine the number of rows and columns
+    import math
+    num_subplots = len(type_change_percentages)
+    num_cols = 4  # fixed for nice visualization
+    num_rows = math.ceil(num_subplots / num_cols)
+
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(30, 6 * num_rows))
+
+    # Flatten axes array in case of multiple rows
+    axes = axes.flatten()
+
+    # Iterate through data and plot
+    for i, (unit_type, percentages) in enumerate(type_change_percentages.items()):
+        ax = axes[i]
+        labels = percentages.keys()
+
+        colors = []
+        for label in labels:
+            if label == 'P':
+                colors.append(plt.cm.Pastel1.colors[1])
+            elif label == 'B':
+                colors.append(plt.cm.Pastel1.colors[0])
+            elif label == 'D':
+                colors.append(plt.cm.Pastel1.colors[2])
+            elif label == 'P+B':
+                colors.append(plt.cm.Pastel1.colors[3])
+            elif label == 'P+D':
+                colors.append(plt.cm.Pastel1.colors[4])
+            elif label == 'B+D':
+                colors.append(plt.cm.Pastel1.colors[5])
+            elif label == 'P+B+D':
+                colors.append(plt.cm.Pastel1.colors[6])
+            elif label == 'No Type':
+                colors.append(plt.cm.Pastel1.colors[7])
+            elif label == 'Inactive':
+                colors.append("grey")
+
+        ax.pie(
+            percentages.values(), 
+            labels=labels, 
+            autopct=lambda p: '{:.1f}'.format(round(p, 1)) if p > 0 else '',
+            explode=[0.1]*len(labels),
+            startangle=0,
+            colors=colors
+        )
+        ax.set_title(f"Original type: {unit_type}")
+
+    # Remove any empty subplots
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
 
     # Save the plot
+    plt.tight_layout()
     plot_path = f'{figs_dir}/unit_type_change_plot_{config_version_1}-{config_version_2}.png'
     plt.savefig(plot_path)
 
 
 def main(configs, experiment, moving_trajectory):
-    _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory)
-    _plot_between_envs_unit_types_change(configs[0], configs[2], experiment, moving_trajectory)
+    # _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory)
+    _plot_between_envs_unit_types_change(configs[0], configs[1], experiment, moving_trajectory)
 
 
 if __name__ == '__main__':
