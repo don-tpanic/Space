e30d6b47d87a177ba17c106fc4665d8aa0ba57d4
Xiaoliang (Ken) Luo <ken@love22.pals.ucl.ac.uk>
2024-07-30 12:44:51 +0100

    cleaned code

--- remapping.py
+++ remapping.py
@@ -56,116 +56,112 @@ def _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory):
     plt.savefig(f"{figs_dir}/changes_in_heatmaps.png")
 
 
-def _plot_between_envs_unit_types_change(config_version_1, config_version_2, experiment, moving_trajectory):
+def _plot_between_envs_unit_types_change(configs, experiment, moving_trajectory):
     """
     For two contrasting envs, compare how unit types change from env_1 to env_2.
     For each unit type in env_1, plot the percentage of units that have changed to each type in env_2.
     """
-    plt.rcParams.update(
-        {
-            'font.size': 22, 
-            'font.family': 'sans-serif',
-            'font.sans-serif': 'Arial',
-        }
-    )
+    plt.rcParams.update({'font.size': 22,})
 
-    config_1 = utils.load_config(config_version_1)
-    results_path_1 = utils.load_results_path(
-        config=config_1,
-        experiment=experiment,
-        moving_trajectory=moving_trajectory,
-    )
-    fpath_1 = f'{results_path_1}/unit_chart.npy'
-    unit_chart_1 = np.load(fpath_1, allow_pickle=True)
-    indices_1 = umc._unit_chart_type_classification(unit_chart_1)
+    for config_version_1 in configs:
+        for config_version_2 in configs:
+            if config_version_1 == config_version_2:
+                continue
 
-    # Get original unit type percentages
-    total_cells_original = sum(len(indices_1[key]) for key in indices_1)
-    type_proportions_original = {}
-    for unit_type, index_list in indices_1.items():
-        proportion = len(index_list) / total_cells_original * 100
-        if proportion > 0:
-            type_proportions_original[unit_types[unit_type]] = proportion
+            config_1 = utils.load_config(config_version_1)
+            results_path_1 = utils.load_results_path(
+                config=config_1,
+                experiment=experiment,
+                moving_trajectory=moving_trajectory,
+            )
+            fpath_1 = f'{results_path_1}/unit_chart.npy'
+            unit_chart_1 = np.load(fpath_1, allow_pickle=True)
+            indices_1 = umc._unit_chart_type_classification(unit_chart_1)
+
+            # Get original unit type percentages
+            total_cells_original = sum(len(indices_1[key]) for key in indices_1)
+            type_proportions_original = {}
+            for unit_type, index_list in indices_1.items():
+                proportion = len(index_list) / total_cells_original * 100
+                if proportion > 0:
+                    type_proportions_original[unit_types[unit_type]] = proportion
 
 
-    config_2 = utils.load_config(config_version_2)
-    results_path_2 = utils.load_results_path(
-        config=config_2,
-        experiment=experiment,
-        moving_trajectory=moving_trajectory,
-    )
-    fpath_2 = f'{results_path_2}/unit_chart.npy'
-    unit_chart_2 = np.load(fpath_2, allow_pickle=True)
-    indices_2 = umc._unit_chart_type_classification(unit_chart_2)
+            config_2 = utils.load_config(config_version_2)
+            results_path_2 = utils.load_results_path(
+                config=config_2,
+                experiment=experiment,
+                moving_trajectory=moving_trajectory,
+            )
+            fpath_2 = f'{results_path_2}/unit_chart.npy'
+            unit_chart_2 = np.load(fpath_2, allow_pickle=True)
+            indices_2 = umc._unit_chart_type_classification(unit_chart_2)
 
-    type_change_percentages = {}
-    # e.g., {"Inactive": {"Inactive": 1, "P+B+D": 2, "P+B": 3, "P+D": 4, "B+D": 5, "P": 6}}
+            type_change_percentages = {}
+            # e.g., {"Inactive": {"Inactive": 1, "P+B+D": 2, "P+B": 3, "P+D": 4, "B+D": 5, "P": 6}}
 
-    for i, unit_type_1 in enumerate(unit_types.keys()):
-        results = {}
-        # e.g.  = {'Inactive': 1, 'P+B+D': 2, 'P+B': 3, 'P+D': 4, 'B+D': 5, 'P': 6}
-        
-        # Count how many units of type_1 changed to each type in env_2
-        for idx in indices_1[unit_type_1]:
-            for unit_type_2 in unit_types:
-                if idx in indices_2[unit_type_2]:
-                    unit_type_2_print = unit_types[unit_type_2]
-                    results[unit_type_2_print] = results.get(unit_type_2_print, 0) + 1
-                    break  # once found, can safety break because each unit can only change to one type
-        
-        # Calculate percentages
-        total_cells = len(indices_1[unit_type_1])
-        if total_cells > 0:
-            percentages = {key: (value / total_cells) * 100 for key, value in results.items()}
-        
-            # Store the percentages
-            type_change_percentages[unit_types[unit_type_1]] = percentages
+            for i, unit_type_1 in enumerate(unit_types.keys()):
+                results = {}
+                # e.g.  = {'Inactive': 1, 'P+B+D': 2, 'P+B': 3, 'P+D': 4, 'B+D': 5, 'P': 6}
+                
+                # Count how many units of type_1 changed to each type in env_2
+                for idx in indices_1[unit_type_1]:
+                    for unit_type_2 in unit_types:
+                        if idx in indices_2[unit_type_2]:
+                            unit_type_2_print = unit_types[unit_type_2]
+                            results[unit_type_2_print] = results.get(unit_type_2_print, 0) + 1
+                            break  # once found, can safety break because each unit can only change to one type
+                
+                # Calculate percentages
+                total_cells = len(indices_1[unit_type_1])
+                if total_cells > 0:
+                    percentages = {key: (value / total_cells) * 100 for key, value in results.items()}
+                
+                    # Store the percentages
+                    type_change_percentages[unit_types[unit_type_1]] = percentages
 
-    # Determine the number of rows and columns
-    import math
-    num_subplots = len(type_change_percentages)
-    num_cols = 4  # fixed for nice visualization
-    num_rows = math.ceil(num_subplots / num_cols)
+            # Determine the number of rows and columns
+            import math
+            num_subplots = len(type_change_percentages)
+            num_cols = 4  # fixed for nice visualization
+            num_rows = math.ceil(num_subplots / num_cols)
 
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(30, 6 * num_rows))
+            fig, axes = plt.subplots(num_rows, num_cols, figsize=(30, 6 * num_rows))
 
-    # Flatten axes array in case of multiple rows
-    axes = axes.flatten()
+            # Flatten axes array in case of multiple rows
+            axes = axes.flatten()
 
-    # Iterate through data and plot
-    for i, (unit_type, percentages) in enumerate(type_change_percentages.items()):
-        ax = axes[i]
-        labels = percentages.keys()
-        colors = [type2color[label] for label in labels]
-        ax.pie(
-            percentages.values(), 
-            labels=labels, 
-            autopct=lambda p: '{:.1f}'.format(round(p, 1)) if p > 0 else '',
-            explode=[0.1]*len(labels),
-            startangle=0,
-            colors=colors
-        )
-        ax.set_title(f"Original: {unit_type}, %= {type_proportions_original[unit_type]:.2f}")
+            # Iterate through data and plot
+            for i, (unit_type, percentages) in enumerate(type_change_percentages.items()):
+                ax = axes[i]
+                labels = percentages.keys()
+                colors = [type2color[label] for label in labels]
+                ax.pie(
+                    percentages.values(), 
+                    labels=labels, 
+                    autopct=lambda p: '{:.1f}'.format(round(p, 1)) if p > 0 else '',
+                    explode=[0.1]*len(labels),
+                    startangle=0,
+                    colors=colors
+                )
+                ax.set_title(f"Original: {unit_type}, %= {type_proportions_original[unit_type]:.2f}")
 
-    # Remove any empty subplots
-    for j in range(i + 1, len(axes)):
-        fig.delaxes(axes[j])
+            # Remove any empty subplots
+            for j in range(i + 1, len(axes)):
+                fig.delaxes(axes[j])
 
-    # Save the plot
-    plt.tight_layout()
-    plot_path = f'{figs_dir}/unit_type_change_plot_{config_version_1}-{config_version_2}.png'
-    plt.savefig(plot_path)
+            # Save the plot
+            plt.tight_layout()
+            plot_path = f'{figs_dir}/unit_type_change_plot_{config_version_1}-{config_version_2}.png'
+            plt.savefig(plot_path)
+            plt.close()
 
 
 def _plot_each_env_cell_type_proportions(configs, experiment, moving_trajectory):
     """
     For each given config, plot the proportion of different cell types as a pie chart.
     """
-    plt.rcParams.update({
-        'font.size': 22, 
-        'font.family': 'sans-serif',
-        'font.sans-serif': 'Arial',
-    })
+    plt.rcParams.update({'font.size': 22,})
 
     num_configs = len(configs)
     num_cols = 5
@@ -223,13 +219,13 @@ def _plot_each_env_cell_type_proportions(configs, experiment, moving_trajectory)
 
 def main(configs, experiment, moving_trajectory):
     _plot_between_envs_unit_heatmaps(configs, experiment, moving_trajectory)
-    _plot_between_envs_unit_types_change(configs[0], configs[2], experiment, moving_trajectory)
+    _plot_between_envs_unit_types_change(configs, experiment, moving_trajectory)
     _plot_each_env_cell_type_proportions(configs, experiment, moving_trajectory)
 
 
 if __name__ == '__main__':
     envs2changes = {
-        "env28_r24_2d_vgg16_fc2": "original",
+        "env28run2_r24_2d_vgg16_fc2": "original",
         "env37_r24_2d_vgg16_fc2": "45 deg",
         "env38_r24_2d_vgg16_fc2": "90 deg",
         "env39_r24_2d_vgg16_fc2": "many items changes",
